3a1a1e94ea1bddcbea9dce17397d3d4f6d4bc4c5
danknjoroge <dank8505@gmail.com>
2022-06-23 09:41:42 +0300

    initial commit

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,27 @@
 const button5 = document.querySelector("#plusFive")
+const btn = document.querySelector("#plusTen")
+const btn1 = document.querySelector("#plusFifteen")
+const btn2 = document.querySelector("#plusTwenty")
+
 const indicator = document.querySelector("#number-indicator")
 const disp = document.querySelector("#display-heading")
 
 button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+})
+
+btn.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+
+btn1.addEventListener('click', () => {
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+
+btn2.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
 })
